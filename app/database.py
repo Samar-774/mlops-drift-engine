ed14529db8de405036f1e7da0a93092ec3250d7b
Samar-774 <mastergogo554@gmail.com>
2026-06-29 21:14:38 +0530

    Switch to PostgreSQL via DATABASE_URL env var

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+import os
 
-DATABASE_URL='sqlite:///./production.db'
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./production.db")
 
-engine= create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
